45cbe0ef0f3aabb49d9422c388c17cce6866d733
Afnan Ferdousi <96898427+AfnanFerdousi@users.noreply.github.com>
2024-11-22 19:48:06 +0600

    Update Supporters.jsx

--- src/components/home/Supporters.jsx
+++ src/components/home/Supporters.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 const Supporters = () => {
     const datas = [
-        "https://i.ibb.co.com/wwqdB5w/large-charlie-min.png",
         "https://i.ibb.co.com/fMGGwy3/bigganchinta.png",
         "https://i.ibb.co.com/3CB0cq5/nctb.png"
     ];

bc6092239749e24af692f9509627db5c9b682a28
AfnanFerdousi <afnanferdousi550@gmail.com>
2024-12-26 03:22:54 +0600

    [DONE] alertBanner

--- src/components/shared/AlertModal.jsx
+++ src/components/shared/AlertModal.jsx
@@ -5,14 +5,16 @@ import ReactPlayer from "react-player";
 import { GrClose } from "react-icons/gr";
 import Lottie from "lottie-react";
 import scrollArrowAnimation from "/public/scrolldown.json";
+import Image from "next/image";
+import { useLanguage } from "@/contexts/LanguageContext";
 
-const AlertBanner = () => {
-  const [isOpen, setIsOpen] = useState(true);
+const AlertBanner = ({setIsOpen, isOpen}) => {
+  const { language } = useLanguage();
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsOpen(true);
-    }, 3000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
@@ -26,14 +28,18 @@ const AlertBanner = () => {
           exit={{ y: -100, opacity: 0 }}
           transition={{ type: "spring", duration: 0.5 }}
           className="absolute top-0 left-0 right-0 z-50 bg-gradient-to-t to-[#f7f7f7] from-[#ffffff] shadow-lg h-screen flex items-center justify-center"
-          style={{
-            backgroundImage: "url('/map.gif')", // Path to your map image
-            backgroundPosition: "center", // Centers the image
-            backgroundRepeat: "no-repeat", // Prevents tiling
-            opacity: 0.2, // Lower the opacity of the map
-            zIndex: -1, // Ensure it stays behind the content
-          }}
         >
+          {/* Backgrounds */}
+          <div
+            className="absolute top-0 left-0 w-full h-full"
+            style={{
+              backgroundImage: "url('/map.gif')",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              opacity: 0.2,
+              zIndex: -1,
+            }}
+          ></div>
           <div
             className="absolute top-0 left-0 w-full h-full content-[''] z-10 pointer-events-none bg-[url('/noise.gif')]"
             style={{ opacity: 0.08 }}
@@ -50,24 +56,28 @@ const AlertBanner = () => {
           {/* Main Content */}
           <main className="relative rounded-lg w-full h-auto max-w-[1440px] p-6 flex flex-col lg:flex-row items-center justify-center gap-8">
             <section className="flex-1 text-center lg:text-left">
-              <p className="text-[#FF7128] text-xl font-bold uppercase mb-2 tracking-wider">
-                #SaveTheChildren
-              </p>
+             <div className="flex items-center gap-2 flex-col md:flex-row">
+  <Image src="/amberalert.png" alt="Logo" width={70} height={70} />
+  <p className="text-[#FF7128] text-xl font-bold uppercase mb-2 sm:mb-0 tracking-wider">
+    #SaveMissingChildren
+  </p>
+</div>
               <h1 className="text-4xl lg:text-6xl font-bold text-red-600 mb-4">
                 ENOUGH IS ENOUGH!
               </h1>
               <h2 className="text-3xl lg:text-4xl font-bold text-gray-800 mb-6">
-                Stand for Change
+                {language == "bn" ? "আওয়াজ তুলুন" : "Stand For Change"}
               </h2>
-              <p className="text-lg lg:text-xl text-gray-700 mb-8">
-                Join us in making a difference. Together, we can create a better
-                future for children around the globe.
+              <p className="text-lg lg:text-xl text-gray-700 md:mb-8 mb-4">
+                {language == "bn"
+                  ? "বাংলাদেশে শিশু নিখোঁজ রোধে যুক্ত হন আমাদের সাথে, পরিবর্তন আসবে আপনাদের থেকেই"
+                  : "Join us in making a difference. Together, we can create a better future for children around the globe."}
               </p>
-              <button className="px-8 py-4 bg-[#FF7128] text-white font-bold text-lg rounded-lg hover:bg-[#e66424] transition-all shadow-lg">
-                Join Us
-              </button>
+              {/* <button className="md:px-8 px-4 md:py-4 py-2 bg-[#FF7128] text-white font-bold md:text-lg text-[16px] rounded-lg hover:bg-[#e66424] transition-all shadow-lg">
+                {language == "bn" ? "যোগ দিন" : "Join Us"}
+              </button> */}
             </section>
-            <section className="flex-1 max-w-full lg:max-w-[60%] rounded-lg overflow-hidden">
+            <section className="flex-1 lg:max-w-[60%] w-full rounded-lg overflow-hidden">
               <ReactPlayer
                 url="https://youtu.be/k_LgAfdeiBA?si=O-Y1YQyQyVanBX1O"
                 controls
@@ -84,20 +94,23 @@ const AlertBanner = () => {
           <div className="absolute bottom-[-40vh] left-1/2 transform -translate-x-1/2">
             <Lottie
               animationData={scrollArrowAnimation}
-              style={{ width: 1000, height: 1000 }}
+              style={{
+                width: "25vw", // Responsive width
+                height: "25vw", // Responsive height
+              }}
               loop
             />
           </div>
 
           {/* Left Ribbon with Sliding Hashtags */}
           <div
-            className="absolute top-0 left-0 w-16 h-full bg-[#FF7128] flex items-center justify-center z-30"
+            className="absolute top-0 left-0 md:w-16 w-10 h-full bg-[#FF7128] flex items-center justify-center z-30 overflow-hidden"
             style={{
               zIndex: 30,
             }}
           >
             <motion.div
-              className="flex flex-col items-center justify-start space-y-28"
+              className="flex flex-col items-center justify-start space-y-28 overflow-hidden h-[80vh]"
               animate={{
                 y: ["0%", "80%"], // Scrolls the text vertically
               }}
@@ -109,37 +122,37 @@ const AlertBanner = () => {
               }}
             >
               <p
-                className="text-white text-sm font-semibold text-center transform rotate-90"
+                className="text-white md:text-[15px] text-sm font-semibold text-center transform rotate-90"
                 style={{ whiteSpace: "nowrap" }}
               >
                 #AmberAlert
               </p>
               <p
-                className="text-white text-sm font-semibold text-center transform rotate-90"
+                className="text-white md:text-[15px] text-sm font-semibold text-center transform rotate-90"
                 style={{ whiteSpace: "nowrap" }}
               >
                 #MissingChildren
               </p>
               <p
-                className="text-white text-sm font-semibold text-center transform rotate-90"
+                className="text-white md:text-[15px] text-sm font-semibold text-center transform rotate-90"
                 style={{ whiteSpace: "nowrap" }}
               >
                 #SaveTheChildren
               </p>
               <p
-                className="text-white text-sm font-semibold text-center transform rotate-90"
+                className="text-white md:text-[15px] text-sm font-semibold text-center transform rotate-90"
                 style={{ whiteSpace: "nowrap" }}
               >
                 #AmberAlert
               </p>
               <p
-                className="text-white text-sm font-semibold text-center transform rotate-90"
+                className="text-white md:text-[15px] text-sm font-semibold text-center transform rotate-90"
                 style={{ whiteSpace: "nowrap" }}
               >
                 #MissingChildren
               </p>
               <p
-                className="text-white text-sm font-semibold text-center transform rotate-90"
+                className="text-white md:text-[15px] text-sm font-semibold text-center transform rotate-90"
                 style={{ whiteSpace: "nowrap" }}
               >
                 #SaveTheChildren

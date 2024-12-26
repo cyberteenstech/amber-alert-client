8ac36e10197c5c98ca078fd31dc35fd88352ae0c
AfnanFerdousi <afnanferdousi550@gmail.com>
2024-12-26 17:59:34 +0600

    [DONE] ui amber alert fix

--- src/components/shared/AlertModal.jsx
+++ src/components/shared/AlertModal.jsx
@@ -4,14 +4,15 @@ import { motion, AnimatePresence } from "framer-motion";
 import ReactPlayer from "react-player";
 import { GrClose } from "react-icons/gr";
 // import Lottie from "lottie-react";
-// import scrollArrowAnimation from "/public/scrolldown.json";
+import scrollArrowAnimation from "/public/scrolldown.json";
 import Image from "next/image";
 import { useLanguage } from "@/contexts/LanguageContext";
 import TypingEffect from 'react-typing-effect';
 import Link from 'next/link';
 import { IoDocumentTextOutline } from 'react-icons/io5';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
-const AlertBanner = ({setIsOpen, isOpen}) => {
+const AlertBanner = ({ setIsOpen, isOpen }) => {
   const { language } = useLanguage();
 
   useEffect(() => {
@@ -58,13 +59,13 @@ const AlertBanner = ({setIsOpen, isOpen}) => {
 
           {/* Main Content */}
           <main className="relative rounded-lg w-full h-auto max-w-[1440px] p-6 flex flex-col lg:flex-row items-center justify-center gap-8">
-            <section className="flex-1 text-center lg:text-left flex flex-col md:justify-start justify-center md:items-start items-center">
-             <div className="flex items-center gap-2 flex-col md:flex-row">
-  <Image src="/amberalert.png" alt="Logo" width={70} height={70} />
-  <p className="text-[#FF7128] text-xl font-bold uppercase mb-2 sm:mb-0 tracking-wider">
-    #SaveMissingChildren
-  </p>
-</div>
+            <section className="flex-1 text-center lg:text-left flex flex-col md:justify-start justify-center md:items-start items-center md:ps-12">
+              <div className="flex items-center gap-2 flex-col md:flex-row ">
+                <Image src="/amberalert.png" alt="Logo" width={70} height={70} />
+                <p className="text-[#FF7128] text-xl font-bold uppercase mb-2 sm:mb-0 tracking-wider">
+                  #SaveMissingChildren
+                </p>
+              </div>
               <h1 className="text-4xl lg:text-6xl font-bold text-red-600 mb-4">
                 ENOUGH IS ENOUGH!
               </h1>
@@ -76,10 +77,10 @@ const AlertBanner = ({setIsOpen, isOpen}) => {
                   ? "বাংলাদেশে শিশু নিখোঁজ রোধে যুক্ত হন আমাদের সাথে, পরিবর্তন আসবে আপনাদের থেকেই"
                   : "Join us in making a difference. Together, we can create a better future for children around the globe."}
               </p>
-               <Link href="#banner" passHref>
-                    <button className="flex items-center gap-x-2 bg-[#FF7128] border-[#FF7128] border-[1.5px] text-[#fff] md:px-6 px-4 py-3 rounded-lg z-30">
-                        <IoDocumentTextOutline className="md:text-xl text-lg" /> {language === "bn" ? "পিটিশন সাক্ষর করুন" : "Sign the petition"}
-                    </button></Link>
+              <Link href="#banner" passHref>
+                <button className="flex items-center gap-x-2 bg-[#FF7128] border-[#FF7128] border-[1.5px] text-[#fff] md:px-6 px-4 py-3 rounded-lg z-30">
+                  <IoDocumentTextOutline className="md:text-xl text-lg" /> {language === "bn" ? "পিটিশন সাক্ষর করুন" : "Sign the petition"}
+                </button></Link>
             </section>
             <section className="flex-1 lg:max-w-[60%] w-full rounded-lg overflow-hidden z-30">
               <ReactPlayer
@@ -94,53 +95,52 @@ const AlertBanner = ({setIsOpen, isOpen}) => {
             </section>
           </main>
 
+
+
           {/* Lottie Animation */}
-          {/* <div className="absolute bottom-[-40vh] left-1/2 transform -translate-x-1/2">
-            <Lottie
-              animationData={scrollArrowAnimation}
-              style={{
-                width: "25vw", // Responsive width
-                height: "25vw", // Responsive height
-              }}
+          <div className="absolute bottom-[-40vh] left-1/2 transform -translate-x-1/2">
+            <DotLottieReact
+              src={scrollArrowAnimation}
               loop
+              autoplay
             />
-          </div> */}
+          </div>
 
           {/* Left Ribbon with Sliding Hashtags */}
-<div
-  className="hidden lg:flex absolute top-0 left-0 md:w-16 w-10 h-full bg-[#FF7128] items-center justify-center z-30 overflow-hidden "
->
-  <div className="flex flex-col items-center justify-center h-full">
-    <div className="text-white text-sm font-bold tracking-wide leading-6 flex flex-col items-center">
-      <TypingEffect
-        text={[
-          "E\nv\ne\nr\ny\n\nS\ne\nc\no\nn\nd\n\nM\na\nt\nt\ne\nr\ns\n\nW\nh\ne\nn\n\n"
-          + "A\n\nC\nh\ni\nl\nd\n\n"
-          + "G\no\ne\ns\n\nM\ni\ns\ns\ni\nn\ng",
-        ]}
-        speed={75} // Typing speed per character
-        eraseSpeed={50} // Erase speed per character
-        typingDelay={500} // Delay before typing starts
-        eraseDelay={3000} // Delay before erasing starts
-        displayTextRenderer={(text, i) => {
-          return (
-            <>
-              {text.split("\n").map((char, index) => (
-                <span
-                  key={index}
-                  className={`${char === "" ? "mb-2" : "block"}`}
-                  style={{ marginBottom: char === "" ? "8px" : "-5px"}}
-                >
-                  {char || "\u00A0"}
-                </span>
-              ))}
-            </>
-          );
-        }}
-      />
-    </div>
-  </div>
-</div>
+          <div
+            className="hidden lg:flex absolute top-0 left-0 md:w-16 w-10 h-full bg-[#FF7128] items-center justify-center z-30 overflow-hidden "
+          >
+            <div className="flex flex-col items-center justify-center h-full">
+              <div className="text-white text-sm font-bold tracking-wide leading-6 flex flex-col items-center">
+                <TypingEffect
+                  text={[
+                    "E\nv\ne\nr\ny\n\nS\ne\nc\no\nn\nd\n\nM\na\nt\nt\ne\nr\ns\n\nW\nh\ne\nn\n\n"
+                    + "A\n\nC\nh\ni\nl\nd\n\n"
+                    + "G\no\ne\ns\n\nM\ni\ns\ns\ni\nn\ng",
+                  ]}
+                  speed={75} // Typing speed per character
+                  eraseSpeed={50} // Erase speed per character
+                  typingDelay={500} // Delay before typing starts
+                  eraseDelay={3000} // Delay before erasing starts
+                  displayTextRenderer={(text, i) => {
+                    return (
+                      <>
+                        {text.split("\n").map((char, index) => (
+                          <span
+                            key={index}
+                            className={`${char === "" ? "mb-2" : "block"}`}
+                            style={{ marginBottom: char === "" ? "8px" : "-5px" }}
+                          >
+                            {char || "\u00A0"}
+                          </span>
+                        ))}
+                      </>
+                    );
+                  }}
+                />
+              </div>
+            </div>
+          </div>
 
         </motion.div>
       )}
